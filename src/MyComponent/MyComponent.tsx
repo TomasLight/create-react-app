2e22f6f19be5c776870d85cbda38cfdb38cd5e53
TomasLight <art.ser.ignatiev@gmail.com>
2020-10-01 18:15:58 +0300

    add typescript to project"

---
+++ src/MyComponent/MyComponent.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface IMyComponentProps {
+    text: string;
+}
+
+type Props = IMyComponentProps;
+
+const MyComponent = (props: Props) => {
+    const { text } = props;
+
+    return (
+        <p style={{ color: "red" }}>
+            {text}
+        </p>
+    );
+};
+
+export { MyComponent };
